f47fa32c51ff9e5d8ef4696adfb12105e3ed8160
mtjlancy <johnlancy98@gmail.com>
2024-09-18 16:04:28 -0400

    smaller images for faster load

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,8 +30,8 @@ module.exports = {
 				},
 			},
 			backgroundImage: {
-				'bg-img-1': "url('/img-1.jpg')",
-				'bg-img-2': "url('/img-2.jpg')",
+				'bg-img-1': "url('/img-1.png')",
+				'bg-img-2': "url('/img-2.png')",
 				'feature-bg': "url('/feature-bg.png')",
 				pattern: "url('/pattern.png')",
 				'pattern-2': "url('/pattern-bg-2.png')",
